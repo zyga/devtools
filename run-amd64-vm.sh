c2f4b005cfc8aa67e47c38be735bc0c04f719f40
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-02-10 19:30:14 +0100

    Use pc-devel.img to run development VMs
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- run-amd64-vm.sh
+++ run-amd64-vm.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+if [ ! -e pc-devel.img ]; then
+    echo "Please make a development PC image first:"
+    echo "$ ./ubuntu-image --developer-mode pc"
+    exit 1
+fi
 sudo kvm \
     -m 512 \
     -nographic \
     -snapshot \
     -redir tcp:8022::22 \
-    pc.img
+    pc-devel.img
